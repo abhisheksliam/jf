03cac4a1b077d8369f3f926f29d7c46a0b547371
Abhishek <abhishek.kumar@comprotechnologies.com>
2016-09-21 11:33:29 +0530

    data cleanup

--- src/test/java/testcase/excel/Test_SKL16_XL_01_01_01_A1.java
+++ src/test/java/testcase/excel/Test_SKL16_XL_01_01_01_A1.java
@@ -1,1 +1,14 @@
-package testcase.word;import org.testng.annotations.Test;import runner.TestRunner;public class Test_GO16_WD_04_4A_01_T1 extends TestRunner {@Test(groups = {"Primary"}) public void GO16_WD_04_4A_01_T1_1() throws Exception {System.out.println("START..");executeItem("GO16.WD.04.4A.01.T1", "T1", "1", "1");executeItem("GO16.WD.04.4A.01.T1", "T1", "2", "1");executeItem("GO16.WD.04.4A.01.T1", "T1", "3", "1");Thread.sleep(3000);System.out.println("DONE.");}}
+package testcase.excel;
+import org.testng.annotations.Test;
+import runner.TestRunner;
+public class Test_SKL16_XL_01_01_01_A1 extends TestRunner {
+    @Test(groups = {
+        "Ribbon"
+    }) public void SKL16_XL_01_01_01_A1_1() throws Exception {
+        System.out.println("START..");
+        executeItem("SKL16.XL.01.01.01.A1", "A1", "1", "1");
+        executeItem("SKL16.XL.01.01.01.A1", "A1", "2", "1");
+        Thread.sleep(3000);
+        System.out.println("DONE.");
+    }
+}
